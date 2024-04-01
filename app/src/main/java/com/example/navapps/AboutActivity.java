2293f0d6ee9b3339e16b8c9e7527536e1ab616c2
Kyyneko <mahendrakirana284@gmail.com>
2024-04-01 22:23:51 +0800

    Pengumpulan Tugas 3 Pemrograman Mobile

---
+++ app/src/main/java/com/example/navapps/AboutActivity.java
@@ -0,0 +1,92 @@
+package com.example.navapps;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class AboutActivity extends AppCompatActivity {
+
+
+    DrawerLayout drawerLayout;
+    ImageView menu;
+    LinearLayout home,settings,share,about,logout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_about);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawerLayout), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        menu = findViewById(R.id.menu);
+        home = findViewById(R.id.home);
+        settings = findViewById(R.id.settings);
+        share = findViewById(R.id.share);
+        about = findViewById(R.id.about);
+        logout = findViewById(R.id.logout);
+
+        menu.setOnClickListener(view -> {
+            openDrawer(drawerLayout);
+        });
+
+        home.setOnClickListener(view -> {
+            redirectActivity(AboutActivity.this, MainActivity.class);
+
+        });
+
+        settings.setOnClickListener(view -> {
+            redirectActivity(AboutActivity.this, SettingsActivity.class);
+
+        });
+
+        share.setOnClickListener(view -> {
+            redirectActivity(AboutActivity.this, ShareActivity.class);
+        });
+
+        about.setOnClickListener(view -> {
+            recreate();
+        });
+
+        logout.setOnClickListener(view -> {
+            Toast.makeText(AboutActivity.this, "Logout", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    public static void openDrawer(DrawerLayout drawerLayout) {
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    public static void redirectActivity(Activity activity, Class secondActivity) {
+        Intent intent = new Intent(activity,secondActivity);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawerLayout);
+    }
+}
